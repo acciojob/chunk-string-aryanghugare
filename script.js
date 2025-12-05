9173e1c49eecce722c79ae2b4a28bb26f90bde3f
Aryan Ghugare <aryanghugare75@gmail.com>
2025-12-06 02:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function stringChop(str, size) {
   // your code here
+	
+	
   let ans = []
   let i = 0 ;
   while(i<str.length){
-    let s = i ;
-    if(s+size<str.length){
-      ans.push(str.substring(i,i+size))
-      i += size ;
+   
+     if(i+size<str.length){
+      ans.push(str.substring(i,i+size)) 
+		 i += size ; 
+     
     }
-    else {
-      ans.push(str.substring(i))
-      return ans ;
+     else {
+      ans.push(str.substring(i));
+		 
+     return ans ;  
     }
-  }
+	   
+  } 
    
   return ans ;
 }
+
+
   
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+ const str = prompt("Enter String.");
+ const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size)); 
